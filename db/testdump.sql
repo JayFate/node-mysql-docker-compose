5911965fa26573b8a6524a3938c97c17a9c39f2e
JayFate <48240828+JayFate@users.noreply.github.com>
2023-08-12 07:35:10 +0000

    update password
    Signed-off-by: JayFate <48240828+JayFate@users.noreply.github.com>

--- db/testdump.sql
+++ db/testdump.sql
@@ -7,6 +7,6 @@ CREATE TABLE Student(
 INSERT INTO Student(student_name, student_age) VALUES("JayFate", 21);
 INSERT INTO Student(student_name, student_age) VALUES("张三", 23);
 
-ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
+ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'root';
 
 flush privileges;
--- web/index.js
+++ web/index.js
@@ -6,8 +6,8 @@ const connection = mysql.createPool({
 	connectionLimit: 10,
 	host: process.env.MYSQL_HOST || 'localhost',
 	user: process.env.MYSQL_USER || 'root',
-	password: process.env.MYSQL_PASSWORD || 'password',
-	database: process.env.MYSQL_DATABASE || 'test'
+	password: process.env.MYSQL_PASSWORD || 'root',
+	database: process.env.MYSQL_DATABASE || 'mydb'
 });
 
 app.get('/', (req, res) => {
